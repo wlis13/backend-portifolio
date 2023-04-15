fc8a697b2c97eda58732a021a4bb9273d797dd19
wlisses <wlissesfernando285@gmail.com>
2023-04-15 17:34:20 -0300

    inserido cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const loginRoutes = require('./routes/login.routes');
 const commentRoutes = require('./routes/comment.routes');
 const registerRoutes = require('./routes/register.routes');
 
 const app = express();
 app.use(express.json());
+app.use(cors({ origin: 'https://backend-portifolio-eight.vercel.app'}));
 
 app.use('/', loginRoutes);
 app.use('/', commentRoutes);
