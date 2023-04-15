4492c347ab571079878d24ba2bd159b60076354a
wlisses <wlissesfernando285@gmail.com>
2023-04-15 10:55:46 -0300

    testando...

--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'testando' });
+})
+
 app.listen(3001, () => console.log('Servidor rodando na porta 3001'));
