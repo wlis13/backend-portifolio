16584b0fba39a6cd9ce9b6ebbc0097f8241cf0c6
wlisses <wlissesfernando285@gmail.com>
2023-04-15 18:26:51 -0300

    retorno dos dados para o localstorage

--- src/controllers/register.controller.js
+++ src/controllers/register.controller.js
@@ -1,10 +1,12 @@
+const loginService = require("../services/login.service");
 const registerUser = require("../services/register.service");
 
 async function registerController(req, res) {
   try {
-    const infoRegister = req.body;
-    const createdRegister = await registerUser(infoRegister);
-    res.status(201).json(createdRegister);
+    const { name } = req.body;
+    await registerUser(req.body);
+    const getNewRegister = await loginService(name);
+    res.status(201).json(getNewRegister);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- src/index.js
+++ src/index.js
@@ -8,12 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: 'http://localhost:3000'}));
 
+const PORT = process.env.MYSQL_PORT;
+
 app.use('/', loginRoutes);
 app.use('/', commentRoutes);
 app.use('/', registerRoutes);
-app.get('/', (_req, res) => {
-  res.status(200).json({ message: 'test' });
-})
 
-
-app.listen(3001, () => console.log('Servidor rodando na porta 3001'));
+app.listen(PORT, () => console.log(`Servidor rodando na porta 3001${PORT}`));
