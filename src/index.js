1c3cefd9e3f32b05c0013e953670cb1c09e11a35
wlisses <wlissesfernando285@gmail.com>
2023-05-12 08:10:39 -0300

    iniciando troca de banco de dados

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const registerRoutes = require('./routes/register.routes');
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:3000'}));
+app.use(cors({ origin: ['http://localhost:3000', 'https://portifolio-one-lilac.vercel.app/']}));
 
 const PORT = process.env.MYSQL_PORT;
 
