e09c98fb02c21056ea185bb71e804b9f1a97068f
wlisses <wlissesfernando285@gmail.com>
2023-06-29 21:11:38 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const registerRoutes = require('./routes/register.routes');
 
 const app = express();
 app.use(express.json());
-app.use(cors('*'));
+app.use(cors());
 
 const PORT = process.env.PORT;
 
