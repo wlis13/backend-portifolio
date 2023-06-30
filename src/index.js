351396f23bf96872e22ead2ad5f9dc9afafcf27a
wlisses <wlissesfernando285@gmail.com>
2023-06-29 21:07:09 -0300

    politica cors

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const registerRoutes = require('./routes/register.routes');
 
 const app = express();
 app.use(express.json());
-app.use(cors('http://localhost:3000'));
+app.use(cors('*'));
 
 const PORT = process.env.PORT;
 
