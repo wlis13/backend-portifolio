a0f1390bdb2af9038219f4949468863986a917ce
wlisses <wlissesfernando285@gmail.com>
2023-07-08 22:35:26 -0300

    retirado exact() de find()

--- src/services/comment.service.js
+++ src/services/comment.service.js
@@ -1,12 +1,22 @@
 const comment = require("../model/tableComment.model");
 
 async function findComments() {
-  const getComments = await comment.find().exact();
-  return getComments;
+  try {
+    const getComments = await comment.find();
+    return getComments;
+  } catch (error) {
+    console.log("Erro ao buscar comentário" + error);
+    throw error;
+  }
 };
 
 async function insertComments(newComment) {
-  await comment.insertMany(newComment);
+  try {
+    await comment.insertMany(newComment);
+  } catch (error) {
+    console.log(`Erro ao tentar inserir novo comentário ${error}`);
+    throw error;
+  }
 };
 
 module.exports = {
